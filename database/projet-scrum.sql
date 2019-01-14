406f080be76ca0e36e61275620c00d47504c4eda
Pichoule <florian.gojon@gmail.com>
2019-01-14 16:06:09 +0100

    database

---
+++ database/projet-scrum.sql
@@ -0,0 +1,237 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  lun. 14 jan. 2019 à 15:50
+-- Version du serveur :  10.1.36-MariaDB
+-- Version de PHP :  7.2.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `projet-scrum`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `events`
+--
+
+CREATE TABLE `events` (
+  `Id` int(11) NOT NULL,
+  `BeginDate` date NOT NULL,
+  `EndDate` date NOT NULL,
+  `Title` varchar(255) COLLATE utf8_bin NOT NULL,
+  `Description` varchar(2000) COLLATE utf8_bin DEFAULT NULL,
+  `isImportant` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `events_users`
+--
+
+CREATE TABLE `events_users` (
+  `Id_event` int(11) NOT NULL,
+  `Id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `files`
+--
+
+CREATE TABLE `files` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `Content` blob NOT NULL,
+  `Id_event` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `groups`
+--
+
+CREATE TABLE `groups` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `groups_users`
+--
+
+CREATE TABLE `groups_users` (
+  `Id` int(11) NOT NULL,
+  `Id_user` int(11) NOT NULL,
+  `Id_group` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `notes`
+--
+
+CREATE TABLE `notes` (
+  `Id` int(11) NOT NULL,
+  `Name` varchar(255) COLLATE utf8_bin NOT NULL,
+  `Content` text COLLATE utf8_bin NOT NULL,
+  `Id-event` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `Id` int(11) NOT NULL,
+  `Lastname` varchar(255) COLLATE utf8_bin NOT NULL,
+  `Firstname` varchar(255) COLLATE utf8_bin NOT NULL,
+  `Email` varchar(255) COLLATE utf8_bin NOT NULL,
+  `Password` varchar(255) COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `events`
+--
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Index pour la table `events_users`
+--
+ALTER TABLE `events_users`
+  ADD KEY `Id_event` (`Id_event`),
+  ADD KEY `Id_user` (`Id_user`);
+
+--
+-- Index pour la table `files`
+--
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `Id_event` (`Id_event`);
+
+--
+-- Index pour la table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Index pour la table `groups_users`
+--
+ALTER TABLE `groups_users`
+  ADD PRIMARY KEY (`Id`),
+  ADD KEY `Id_User` (`Id_user`),
+  ADD KEY `Id_group` (`Id_group`);
+
+--
+-- Index pour la table `notes`
+--
+ALTER TABLE `notes`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`Id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `events`
+--
+ALTER TABLE `events`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `files`
+--
+ALTER TABLE `files`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `groups_users`
+--
+ALTER TABLE `groups_users`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `notes`
+--
+ALTER TABLE `notes`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `events_users`
+--
+ALTER TABLE `events_users`
+  ADD CONSTRAINT `events_users_ibfk_1` FOREIGN KEY (`Id_event`) REFERENCES `events` (`Id`),
+  ADD CONSTRAINT `events_users_ibfk_2` FOREIGN KEY (`Id_user`) REFERENCES `users` (`Id`);
+
+--
+-- Contraintes pour la table `files`
+--
+ALTER TABLE `files`
+  ADD CONSTRAINT `files_ibfk_1` FOREIGN KEY (`Id`) REFERENCES `events` (`Id`);
+
+--
+-- Contraintes pour la table `groups_users`
+--
+ALTER TABLE `groups_users`
+  ADD CONSTRAINT `groups_users_ibfk_1` FOREIGN KEY (`Id_group`) REFERENCES `groups` (`Id`),
+  ADD CONSTRAINT `groups_users_ibfk_2` FOREIGN KEY (`Id_user`) REFERENCES `users` (`Id`);
+
+--
+-- Contraintes pour la table `notes`
+--
+ALTER TABLE `notes`
+  ADD CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`Id`) REFERENCES `events` (`Id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
