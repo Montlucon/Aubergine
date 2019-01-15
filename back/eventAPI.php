6147561aa1dc1323187d974b343339e54886f533
valsov <valentin.sovignet@epsi.fr>
2019-01-15 10:13:53 +0100

    Create event JS and API PHP

--- back/eventAPI.php
+++ back/eventAPI.php
@@ -4,6 +4,10 @@
  * Handle POST requests for the events
  */
 
+    if(isset($_POST["date"]) && isset($_POST["title"]) && isset($_POST["description"]) && isset($_POST["important"])){
+        // do post insert
+        
+    }
 
 
 ?>
