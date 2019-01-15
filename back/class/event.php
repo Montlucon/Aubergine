0a9a9e0986e0b7c32241c2e3112d975df4f2ae51
Romain Guarnotta <guarnotta.romain@gmail.com>
2019-01-15 09:36:26 +0100

    Event class Structure

---
+++ back/class/event.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Event management
+ *
+ * @author Romain
+ */
+require_once 'maBDD.php';
+
+class event {
+
+    private $id;
+    private $beginDate;
+    private $endDate;
+    private $title;
+    private $description;
+    private $isImportante;
+
+    /**
+     * Get event by ID
+     * @param int $id
+     * @return event Event
+     */
+    public function get($id) {
+        
+    }
+
+    // TODO GET/SET
+
+}
