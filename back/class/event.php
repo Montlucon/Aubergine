e9bff4120595b709f3797c077622b1f60da5e52b
Anael MOBILIA <contrib@anael.eu>
2019-01-15 09:37:34 +0100

    Generate getter & setter

--- back/class/event.php
+++ back/class/event.php
@@ -25,6 +25,52 @@ class event {
         
     }
 
-    // TODO GET/SET
+    function getId() {
+        return $this->id;
+    }
+
+    function getBeginDate() {
+        return $this->beginDate;
+    }
+
+    function getEndDate() {
+        return $this->endDate;
+    }
+
+    function getTitle() {
+        return $this->title;
+    }
+
+    function getDescription() {
+        return $this->description;
+    }
+
+    function getIsImportante() {
+        return $this->isImportante;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setBeginDate($beginDate) {
+        $this->beginDate = $beginDate;
+    }
+
+    function setEndDate($endDate) {
+        $this->endDate = $endDate;
+    }
+
+    function setTitle($title) {
+        $this->title = $title;
+    }
+
+    function setDescription($description) {
+        $this->description = $description;
+    }
+
+    function setIsImportante($isImportante) {
+        $this->isImportante = $isImportante;
+    }
 
 }
